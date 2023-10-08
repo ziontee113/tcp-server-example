c05c2f1a309bb342161ae62fdfe22b1747572227
ziontee113 <ziontee113@gmail.com>
2023-10-09 00:43:05 +0700

    Add on connect & on close events

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,22 @@ import net from "net";
 const portNumber = 8081;
 
 const server = net.createServer((socket) => {
+  // Get the remote address and port of the connecting client
+  const remoteAddress = socket.remoteAddress;
+  const remotePort = socket.remotePort;
+
+  console.log(`Client connected from ${remoteAddress}:${remotePort}`);
+
+  // code
   socket.on("data", (data) => {
     const stringData = data.toString();
 
     console.log(`Received: ${stringData}`);
+  });
+
+  // On close
+  socket.on("close", () => {
+    console.log(`Client disconnected from ${remoteAddress}:${remotePort}`);
   });
 });
 
