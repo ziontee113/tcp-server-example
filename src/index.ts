282cd0d3cdc54fddb8dacdc931133a4e3a7b5371
ziontee113 <ziontee113@gmail.com>
2023-10-09 01:49:50 +0700

    Handle incoming sockets from nvim

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,57 @@ import net from "net";
 const portNumber = 8081;
 
 const server = net.createServer((socket) => {
-  // Get the remote address and port of the connecting client
+  // On connect
   const remoteAddress = socket.remoteAddress;
   const remotePort = socket.remotePort;
 
   console.log(`Client connected from ${remoteAddress}:${remotePort}`);
 
-  // code
+  // On data
   socket.on("data", (data) => {
     const stringData = data.toString();
+    const parsedData = JSON.parse(stringData);
+
+    if (parsedData.type === "connection-request-from-lua") {
+      handleIncomingConnectionFromNeovim(
+        `${remoteAddress}:${remotePort}`,
+        parsedData.port
+      );
+    }
 
     console.log(`Received: ${stringData}`);
   });
 
   // On close
   socket.on("close", () => {
+    handleClosingConnectionFromNeovim(`${remoteAddress}:${remotePort}`);
     console.log(`Client disconnected from ${remoteAddress}:${remotePort}`);
   });
 });
 
 server.listen(portNumber);
 console.log(`Listening on port ${portNumber}`);
+
+// ========================================================
+
+let activeNeovimPort: null | number = null;
+let sockets: { [key: string]: net.Socket } = {};
+
+function handleIncomingConnectionFromNeovim(key: string, port: number) {
+  activeNeovimPort = port;
+
+  if (!sockets[activeNeovimPort]) {
+    const newSocket = new net.Socket();
+    newSocket.connect(8081, "0.0.0.0", () => {
+      // TODO: callback
+    });
+    sockets[key] = newSocket;
+  }
+
+  console.log(Object.keys(sockets));
+}
+
+function handleClosingConnectionFromNeovim(key: string) {
+  delete sockets[key];
+  console.log(Object.keys(sockets));
+}
